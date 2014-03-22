89e2a109da9e6d41d46da7b4460bb23172add0c9
Jase Boyfriend <jasonhfields@gmail.com>
2014-03-22 18:33:29 -0500

    Experimenting with sprites and color keys

--- sub-squares/workscreen/main.py
+++ sub-squares/workscreen/main.py
@@ -15,14 +15,32 @@ pygame.init()
 screen = pygame.display.set_mode((640,480))
 clock = pygame.time.Clock()
 
+class Circe(pygame.sprite.Sprite):
+    image = pygame.Surface((100,100))
+    image.set_colorkey((3,0,0))
+    pygame.draw.circle(image, (255,0,0), (50,50), 50, 2)
+    image.convert_alpha()
 
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = Circe.image
+        self.rect = self.image.get_rect()
+        self.radius = 50
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+sprites = pygame.sprite.Group()
+c = Circe()
+sprites.add(c)
 while 1:
     screen.fill((240,240,240))
 
+    sprites.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    sprites.draw(screen)
     pygame.display.flip()
     clock.tick(60)
