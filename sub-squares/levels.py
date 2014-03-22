1f131047cb6904d2cb098b28a58d6dd091455a35
Jase Boyfriend <jasonhfields@gmail.com>
2014-03-21 19:19:11 -0500

    Broke the code up into modules, much better

---
+++ sub-squares/levels.py
@@ -0,0 +1,204 @@
+from __future__ import print_function
+from constants import *
+import pygame
+
+#######################################
+#                                     #
+#    Platform                         #
+#                                     #
+#######################################
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface(size)
+        self.image.fill(WALLCOLOR)
+
+        self.rect = self.image.get_rect()
+
+
+#######################################
+#                                     #
+#       Power Up Blocks               #
+#                                     #
+#######################################
+class GravityBlock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface((32,32))
+        self.blue = 100 # used in color fading
+        self.blueup = True # used for direction in color fading
+        self.image.fill((20, 45, self.blue))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Fades color brightness in and out"""
+        if self.blueup:
+            self.blue += 8
+            if self.blue >= 245:
+                self.blueup = False
+        else:
+            self.blue -= 8
+            if self.blue <= 100:
+                self.blueup = True
+
+        self.image.fill((10, 25, self.blue))
+
+class SpeedBlock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface((32,32))
+        self.green = 100
+        self.greenup = True
+        self.image.fill((180, self.green, 20))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        if self.greenup:
+            self.green += 8
+            if self.green >= 245:
+                self.greenup = False
+        else:
+            self.green -= 8
+            if self.green <= 100:
+                self.greenup = True
+
+        self.image.fill((180, self.green, 20))
+
+
+#######################################
+#                                     #
+#           Level Base Class          #
+#                                     #
+#######################################
+class Level(object):
+    platform_list = None
+    enemy_list = None
+    special_blocks = None
+
+    background = None
+    world_shift = 0
+
+    def __init__(self):
+
+        self.platform_list = pygame.sprite.Group() #group for platforms
+        self.enemy_list = pygame.sprite.Group() # group for enemies.. not yet used
+        self.special_blocks = pygame.sprite.Group() #group for gravity blocks
+        self.bullet_list = pygame.sprite.Group()
+        self.bgrect = pygame.Rect((0,0,0,0)) #needs to be initialized for now
+
+    def update(self):
+        self.platform_list.update() #call update on every platform object
+        self.enemy_list.update() # and every enemy object ... not yet used though--so empty
+        self.special_blocks.update() #update special blocks
+        self.bullet_list.update() #might be able to remove all of these
+        #self.all_sprites_list.update()
+
+    def shift_world(self, shift_x):
+        self.world_shift += shift_x
+        for platform in self.platform_list:
+            platform.rect.x += shift_x
+        for enemy in self.enemy_list:
+            enemy.rect.x += shift_x
+        for block in self.special_blocks:
+            block.rect.x += shift_x
+        self.bgrect.x += shift_x
+
+#######################################
+#                                     #
+#        Actual Levels                #
+#                                     #
+#######################################
+class Level01(Level):
+    def __init__(self):
+        Level.__init__(self)
+        self.level_limit_r = -1000
+        self.level_limit_l = 1000
+        self.bglist = []
+        self.bgimglist = ['../images/lv1bg1sky.png', '../images/lv1bg2mntfar.png', '../images/lv1bg3mntmid.png', '../images/lv1bg4mntnear.png']
+
+        ##########################
+        #  background images     #
+        ##########################
+        self.bgimage = pygame.image.load('../images/level1bg.png')
+        self.bgrect = self.bgimage.get_rect(x=-self.level_limit_l, y=0)
+
+        # a list of platforms........
+        #           width  X     Y  of top left
+        platforms = [[210, 500, 500],
+                [210, 200, 400],
+                [210, 600, 300],
+                [210, 100, 100],
+                [420, 1000, SCREENH/2],
+                [600,   0,  SCREENH-PLATFORMH]
+                ]
+        for platform in platforms:  #for each element of level, create a platform
+            block = Platform((platform[0], PLATFORMH))
+            block.rect.x = platform[1]
+            block.rect.y = platform[2]
+            self.platform_list.add(block) #and add it to platform list
+
+        #Create gravity block and add it to the special_blocks list
+        gravityblock = GravityBlock()
+        gravityblock.rect.x = 200
+        gravityblock.rect.y = 336
+
+        speedblock = SpeedBlock()
+        speedblock.rect.x = 600
+        speedblock.rect.y = 250
+        self.special_blocks.add(gravityblock, speedblock)
+        #self.all_sprites_list.add(gravityblock, speedblock)
+
+    def draw(self):
+
+        SCREEN.blit(self.bgimage, self.bgrect)
+        self.platform_list.draw(SCREEN)
+        self.enemy_list.draw(SCREEN)
+        self.special_blocks.draw(SCREEN)
+        self.bullet_list.draw(SCREEN)
+        #self.all_sprites_list.draw(SCREEN)
+
+class Level02(Level):
+    def __init__(self):
+        Level.__init__(self)
+        self.level_limit_r = -1000
+        self.level_limit_l = 1000
+
+        level = [[110, 70, 520, 400],  #a list of platform rect-style  lists
+                [110, 70, 200, 400],
+                [110, 70, 600, 300],
+                [110, 70, 100, 100],
+                [120, 70, 1000, SCREENH/2],
+                ]
+        for platform in level:  #for each element of level, create a platform
+            block = Platform((platform[0], platform[1]))
+            block.rect.x = platform[2]
+            block.rect.y = platform[3]
+            self.platform_list.add(block) #and add it to platform list
+
+        #Create gravity block and add it to the special_blocks list
+        gravityblock = GravityBlock()
+        gravityblock.rect.x = 250
+        gravityblock.rect.y = 336
+
+        #creates a speed powerup block and adds it to the list
+        speedblock = SpeedBlock()
+        speedblock.rect.x = -100
+        speedblock.rect.y = SCREENH - 77
+        self.special_blocks.add(gravityblock, speedblock)
+
+        #self.all_sprites_list.add(gravityblock, speedblock)
+
+    def draw(self):
+
+        SCREEN.fill(GREEN)
+        self.platform_list.draw(SCREEN)
+        self.enemy_list.draw(SCREEN)
+        self.special_blocks.draw(SCREEN)
+        self.bullet_list.draw(SCREEN)
+        #self.all_sprites_list.draw(SCREEN)
+
+
+
---
+++ sub-squares/player.py
@@ -0,0 +1,165 @@
+from __future__ import print_function
+from constants import *
+import pygame
+from levels import GravityBlock, SpeedBlock #This will hopefully be changed so that it doesn't need to do this
+from time import time
+
+#######################################
+#                                     #
+#     Player                          #
+#                                     #
+#######################################
+class Player(pygame.sprite.Sprite):
+
+    #initialize these variables
+    change_x = 0
+    change_y = 0
+    level = None
+
+    def __init__(self, pos):
+        pygame.sprite.Sprite.__init__(self)
+
+
+        #player image
+        self.image = pygame.image.load('../images/trent.png')
+        #self.image = pygame.Surface(PLAYERSIZE)
+        #self.image.fill(RED)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+        self.facing = 'right'
+        self.effects = []
+
+        #At 1, this is essentially inactive. to slow, lower than 1, to speed up, higher than 1
+        self.speedmodifier = 1
+
+        #default gravity amount
+        self.grav_amount = .35
+
+    def update(self):
+        ###move player###
+        #gravity
+        self.calc_grav(self.grav_amount)
+        #left/right
+        self.rect.x += self.change_x
+
+        ###check for collision with gravity_blocks###
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.special_blocks, False)
+        for block in block_hit_list:
+
+            ###Gravity Powerup Block###
+            if isinstance(block, GravityBlock):
+                self.grav_start = time() #Start timer for effect
+                self.grav_amount = .15   #Status effected
+                self.effects.append('low_gravity') #Append to effects list
+                self.level.special_blocks.remove(block) #Remove the block you got
+
+            ###Speed Powerup Block###
+            if isinstance(block, SpeedBlock):
+                self.speed_start = time() #Start timer for effect
+                self.speedmodifier = 2    #Status affected
+                self.effects.append('speed') #Append to effects list
+                self.level.special_blocks.remove(block) #Remove the block you got
+
+        ###check for collisions with platforms###
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
+            if self.change_x > 0: #if moving right
+                self.rect.right = block.rect.left #set the right edge of the player to the left edge fo the block you collided with
+            elif self.change_x < 0: #if moving left
+                self.rect.left = block.rect.right #do the inverse
+
+        #up/down, player's y position changes based on value of self.change_y
+        self.rect.y += self.change_y
+
+        ###check for collisions with platforms again... why?###
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
+            if self.change_y > 0:#if moving down
+                self.rect.bottom = block.rect.top #set the bottom of the player rect to the top of the block you collided with
+            elif self.change_y < 0:#if moving up
+                self.rect.top = block.rect.bottom#do the inverse
+
+            self.change_y = 0 #reset self.change_y value so that you aren't moving up anymore if jumping and gravity can take its course
+
+
+        ###check for status effects###
+        if self.effects:
+            for effect in self.effects:
+                if effect == 'speed':
+                    if time() - self.speed_start > 5: #checks for five seconds since the effect started, if it has been longer than 5 seconds...
+                        self.speedmodifier = 1  #reset speed
+                        self.effects.remove(effect)     #remove effect
+
+                if effect == 'low_gravity':          #same model as above..
+                    if time() - self.grav_start > 5:
+                        self.grav_amount = .35
+                        self.effects.remove(effect)
+
+    def calc_grav(self, amount):
+        if self.change_y == 0:#if change_y is nothing...
+            self.change_y = 1 #keep pushing player down one...?
+        else:
+            self.change_y += amount #otherwise, if change_y is anything, add .35 to it (positive numbers represent downward motion)
+
+        #if bottom of player is outside of screen and still moving down...
+        if self.rect.y >= SCREENH - self.rect.height and self.change_y >= 0:
+            self.change_y = 0 #stop y motion
+            self.rect.y = SCREENH - self.rect.height #set player's bottom edge on bottom of screen
+
+    def jump(self):
+
+        #moves down two px to check for platform collision below
+        self.rect.y += 2
+        #should collide if platform is below
+        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        self.rect.y -= 2 #moves back up
+
+        #if collided with a platform or if you're at the bottom of the screen, then you are allowed to jump
+        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREENH:
+            self.change_y = -10 #jump.. negative
+
+    #player controlled movement
+    def go_left(self):
+        self.change_x = -6 * self.speedmodifier
+        if self.facing == 'right':
+            self.image = pygame.transform.flip(self.image, 1, 0) #flips image based on direction
+            self.facing = 'left'
+
+    def go_right(self):
+        self.change_x = 6 * self.speedmodifier
+        if self.facing == 'left':
+            self.image = pygame.transform.flip(self.image, 1, 0) #same thing as above
+            self.facing = 'right'
+
+    def stop(self):
+        self.change_x = 0
+
+    def stopjump(self):
+        self.change_y = self.change_y / 2 #contols the way the jump handles when you release space bar
+
+#######################################
+#                                     #
+#    Projectiles                      #
+#                                     #
+#######################################
+class Bullet(pygame.sprite.Sprite):
+    """A class for Bullet objects"""
+    def __init__(self, owner):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.owner = owner
+        #To get the constant direction of the bullet, get the direction that the person who shot it was facing when they did
+        self.direction = self.owner.facing
+        self.image = pygame.Surface(BULLETSIZE) #4,10 is the size
+        self.image.fill(BULLETCOLOR) #a blackish color
+        self.rect = self.image.get_rect()
+
+    def update(self): #moves bullet in direction
+        if self.direction == 'right':
+            self.rect.x += BULLETSPEED
+        else:
+            self.rect.x -= BULLETSPEED
+
